232f92a69392281be654b2f4d11e68d0c93c1ac4
brady-bates <batesbradym@gmail.com>
2025-03-23 20:32:51 -0500

    Add seeList

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,16 +8,20 @@ fun main() {
 //    true
     false
 
+  val seedList = mutableListOf(
+    "1111110101001111111111011", // idk
+    "1111011110010101110110010" // 3-cycle oscillation
+  )
+
   if (testing) Settings.maxIterations = 1 // -1
 
   Settings.seedSize = 8 // 5
-//  Settings.seed =
-//  "1111110101001111111111011"
+  Settings.seed = seedList[1]
 //  Settings.stretchToTerminal = true // false
 //  Settings.backgroundOn = true
 //  Settings.numberOfIterations = 1 // -1
   Settings.height = 239 // 45
   Settings.width = 55 // 100
-  Settings.updateDelayMS = 2
+  Settings.updateDelayMS = 200
   mainLoop()
 }
