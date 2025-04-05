735c5b61f3bcffbd32cdd4ce777af6e14fbb4663
brady-bates <batesbradym@gmail.com>
2025-04-04 22:59:37 -0500

    Clean up settings more

--- src/main/kotlin/gameoflife/Main.kt
+++ src/main/kotlin/gameoflife/Main.kt
@@ -12,25 +12,22 @@ fun main() {
     "idk" to "1111110101001111111111011",
     "3-cycle oscillation" to "1111011110010101110110010",
   )
-  
+
   Settings.apply {
-    // Comments are the default value
+    // Comments represent the default value
     seedSize = 8     // 5
     height = 45      // 45
     width = 100      // 100
 
-//    aliveString = "\uD83D\uDD25"    // @
-//    deadString = "-"                // .
-//    backgroundOn = true             // false
-//    stretchToTerminal = true        // false
+    aliveString = "\uD83D\uDD25"    // @
+    deadString = "-"                // .
+    backgroundOn = true             // false
 
     updateDelayMS = Settings.SpeedsInMS.FAST.speed    // MEDIUM (150ms)
-    //    Settings.numberOfIterations = 1               // Run until finished
-    //    seed = seedList[]                             // Random Seeds
+//    seed = seedList[]                                 // Random Seeds
+
+    if (testing) maxIterations = 1 // No limit
   }
-
-
-  if (testing) Settings.maxIterations = 1 // -1
 
   Game.mainLoop()
 }
